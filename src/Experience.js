fe0af378cc6843c2d432f84a8abe6d025e8abb89
yogalakshmi <iamyogalakshmi@gmail.com>
2022-04-12 23:55:32 +0530

    Experience section over

--- src/Experience.js
+++ src/Experience.js
@@ -1,22 +1,64 @@
 import React from "react";
 import './Experience.scss';
+import Code from './Images/expcode.png';
+import GD from './Images/expgd.png';
+import MGD from './Images/expmgd.png';
+import UIUX from './Images/expuiux.png';
 
+
+const data = [
+    {
+        title :'2019',
+        img : Code,
+        data : 'React Js',
+        data2 : '(HTML, CSS)'
+    },
+    {
+        title :'2020',
+        img : GD,
+        data : 'Graphic',
+        data2 : 'Design'
+    },
+    {
+        title :'2021',
+        img : MGD,
+        data : 'Motion Graphic',
+        data2 : 'Design'
+    },
+    {
+        title :'2021 - 2022',
+        img : UIUX,
+        data : 'UI/UX',
+        data2 : 'Designing'
+    },
+]
 
 const Exp = () => (
     <div className="exp-section">
         <div className="exp">
 
             <div className="title">
-
+                <p>Experience</p>
+                <div className="line"></div>
+               
             </div>
 
             <div className="years">
-                <div className="">
-                    <h2>2019</h2>
-                    <image></image>
-                    <p></p>
+            {
+                data.map (p =>
+                <div className="expdata">
+                   
+                    <h2>{p.title}</h2>
+                    <img src={p.img} alt="" />
+                    <p>{p.data}</p>
+                    <p>{p.data2}</p>
+                   
                 </div>
+                )
+            }
             </div>
+
+            <div className="line2"></div>
 
         </div>
     </div>
---
+++ src/Whatido.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './Whatido.scss';
+
+const Whatido = () => (
+    <div className="work-section">
+        <div className="work">
+
+            <div className="whatido">
+                <p></p>
+                <div className="line"></div>
+                <ul>
+
+                </ul>
+            </div>
+
+            <div className="eduction">
+                <p></p>
+                <div className="line"></div>
+                
+            </div>
+        </div>
+    </div>
+);
+
+export default Whatido;
