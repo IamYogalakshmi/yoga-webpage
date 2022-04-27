0dac3f7995c5ddb0acc137b15e096377d7445bff
yogalakshmi <iamyogalakshmi@gmail.com>
2022-04-28 00:25:04 +0530

    added images to motion graphic

--- src/MotionGraphic.js
+++ src/MotionGraphic.js
@@ -2,6 +2,9 @@ import React from "react";
 import './MotionGraphic.scss';
 import logo from './Images/youtube.png';
 import { ExternalLink } from 'react-external-link';
+import image1 from './Images/youtubexperdes.png';
+import image2 from './Images/youtubevaccine.png';
+import image3 from './Images/youtubeventilator.png';
 
 const MotionGraphic = () => (
     <div className="motion-graphic">
@@ -14,23 +17,36 @@ the graphic Designing tools used for explainer videos and intro videos </h3>
 
             <div className="youtube">
 
-                <div className="box"> 
-                     <ExternalLink href="https://www.youtube.com/watch?v=eUCzIQcZ-zY">
-                        <img src={logo} alt=""></img> 
-                     </ExternalLink>
-                </div>
+              <ExternalLink href="https://www.youtube.com/watch?v=eUCzIQcZ-zY">
+                    <div className="box" style={{  
+                                            backgroundImage: "url(" + image1 + ")", 
+                                            backgroundSize: 'cover',
+                                                }}> 
+                            <img src={logo} alt=""></img> 
+                        
+                    </div>
+                </ExternalLink>
 
-                <div className="box"> 
-                     <ExternalLink href="https://www.youtube.com/watch?v=EkmD-gueGbE&t=261s/">
-                       <img src={logo} alt=""></img> 
-                     </ExternalLink>
-                </div>
+                <ExternalLink href="https://www.youtube.com/watch?v=EkmD-gueGbE&t=261s/">
+                    <div className="box" style={{  
+                                            backgroundImage: "url(" + image2 + ")", 
+                                            backgroundSize: 'cover',
+                                               }}> 
+                            <img src={logo} alt=""></img> 
+                        
+                    </div>
+                </ExternalLink>
 
-                <div className="box"> 
-                    <ExternalLink href="https://www.youtube.com/watch?v=LM3PWJ6jT6s&t=242s">
-                        <img src={logo} alt=""></img> 
-                    </ExternalLink>
-                </div>
+                <ExternalLink href="https://www.youtube.com/watch?v=LM3PWJ6jT6s&t=242s">                               
+                    <div className="box" style={{  
+                                            backgroundImage: "url(" + image3 + ")", 
+                                            backgroundSize: 'cover', 
+                                                }}>
+                    
+                            <img src={logo} alt=""></img> 
+                    
+                    </div>
+                </ExternalLink>
                 
             </div>
         </div>
